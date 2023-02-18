7f2eef439cfdedfd3e050459886c205e611652ca
Krissada Sarawit <topfeekrissada@outlook.com>
2023-02-19 06:49:47 +0700

    add exercise 04

---
+++ exercise-004/readme.md
@@ -0,0 +1,17 @@
+ใช้ web worker ในการเปลี่ยนสีของเวลา และ Stick ติดขอบบนขวาจอ
+เมื่อกดที่เวลา เวลาจะหยุดและไม่เปลี่ยนสี
+เมื่อเอาเมาส์ชี้ใช้ eventOnMouseHover เปลี่ยนสี
+ - **เมื่อคลิกแล้วมี name surname id นศ. Subject : Software Studio Semester : 2/2565
+ - เมื่อคลิกอีกครั้งซ่อนกลับไป
+
+<H1> Snapshots
+ใช้ <li> เมื่อคลิกก็จะเปลี่ยนภาพ Opening Picture
+- Firework
+
+Opening Picture ใช้รูปอะไรก็ได้ 
+
+
+=======
+Submit 2 file, HTML และ Javascript
+submit ใน goedu ได้ภายในวันที่ 23
+ส่งได้ 2 วัน
---
+++ exercise-004/worker.js
@@ -0,0 +1,36 @@
+// This worker is used to generate a color for each new message received.
+const COLORS = [
+    "#FFC6D9",
+    "#FFB347",
+    "#B39EB5",
+    "#77DD77",
+    "#FDFD96",
+    "#FF6961",
+    "#CFCFC4",
+    "#AEC6CF",
+    "#F49AC2",  
+    "#C5E384",  
+
+    "#FF1493",
+    "#00FF00",
+    "#FFFF00",
+    "#FFD700",
+    "#00FFFF",
+    "#FF00FF",
+    "#8A2BE2",
+    "#00FA9A",
+    "#FFA07A",
+    "#FF4500"
+];
+
+let currentColorIndex = 0;
+
+addEventListener("message", (event) => {
+    const colorIndex = event.data;
+    const backgroundColor = COLORS[currentColorIndex];
+    const foregroundColor = COLORS[(currentColorIndex + 10) % COLORS.length];
+    currentColorIndex += 1;
+    currentColorIndex %= COLORS.length;
+    const color = {"backgroundColor": backgroundColor, "foregroundColor": foregroundColor}
+    postMessage(color);
+});
